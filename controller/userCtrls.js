7d0791da43984d4832c2abb942ebdf0ef7dbf90c
KessNelly <ojkessnelly@gmail.com>
2023-08-06 16:43:34 +0100

    first commit

---
+++ controller/userCtrls.js
@@ -0,0 +1,23 @@
+const User = require ('../models/userModel');
+
+//Create new User
+const createUser = async (req, res)=>{
+    const email = req.body.email;
+    const findUser = await User.findOne({email: email});
+    if(!findUser){
+       // create new user
+       const newUser = await User.create(req.body)
+       res.json(newUser);
+    }
+    else{
+       // user already exists
+       res.json({
+        msg:'User already exists',
+        success: false
+       })
+    }
+    
+   
+}
+
+module.exports = {createUser};
