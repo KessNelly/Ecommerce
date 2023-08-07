d9e46a94c2d141aeab2c79dce3c96d4164ccdfa9
KessNelly <ojkessnelly@gmail.com>
2023-08-07 23:33:36 +0100

    added error handling

--- controller/userCtrls.js
+++ controller/userCtrls.js
@@ -1,23 +1,24 @@
 const User = require ('../models/userModel');
+const asyncHandler = require("express-async-handler")
 
 //Create new User
-const createUser = async (req, res)=>{
-    const email = req.body.email;
-    const findUser = await User.findOne({email: email});
-    if(!findUser){
-       // create new user
-       const newUser = await User.create(req.body)
-       res.json(newUser);
-    }
-    else{
-       // user already exists
-       res.json({
-        msg:'User already exists',
-        success: false
-       })
-    }
-    
-   
-}
+const createUser = asyncHandler(
+   async (req, res)=>{
+      const email = req.body.email;
+      const findUser = await User.findOne({email: email});
+      if(!findUser){
+         // create new user
+         const newUser = await User.create(req.body)
+         res.json(newUser);
+      }
+      else{
+         // user already exists
+         // res.json({
+         //  msg:'User already exists',
+         //  success: false
+         // })
+         throw new Error('user already exists')
+      }
+   })
 
 module.exports = {createUser};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const dbConnect = require('./config/dbConnect');
+const  {notFound, errorHandler} = require('./middlewares/errorHandler')
 const app = express();
 const dotenv = require ('dotenv').config();
 const PORT = process.env.PORT || 4010
@@ -10,6 +11,12 @@ dbConnect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/api/user', authRouter);
+
+app.use(notFound)
+app.use(errorHandler);
+
+
+
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
 })
